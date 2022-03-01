2ffda5fbd6c66897ef30ea016c732138b4e013db
William Namm <WNamm7@gmail.com>
2022-03-01 11:42:23 -0500

    Added R package framework

---
+++ AutoMetaAnalysis/R/hello.R
@@ -0,0 +1,24 @@
+#' @title Hello, World!
+#'
+#' @description This is an example function named 'hello' which prints 'Hello, World!'.
+#'
+#' @param string Describe a parameter here.
+#'
+#' @return This is the return value.
+#' @export
+#'
+#' @details You can learn more about package authoring with RStudio at:
+#'
+#'    http://r-pkgs.had.co.nz/
+#'
+#'    Some useful keyboard shortcuts for package authoring:
+#'
+#'    Install Package:           'Ctrl + Shift + B'
+#'
+#'    Check Package:             'Ctrl + Shift + E'
+#'
+#'    Test Package:              'Ctrl + Shift + T'
+
+hello <- function() {
+  print("Hello, World!")
+}
